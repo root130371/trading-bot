503a73a6553bacd52f18fa8dd76fbe42d04a296a
Aral <alphacharliebetadelta1@gmail.com>
2025-10-26 23:30:20 +0800

    atr tp set to 1.5

--- pinev4.py
+++ pinev4.py
@@ -63,7 +63,7 @@ market = exchange.market(SYMBOL)
 # Leverage setting
 LEVERAGE = 50
 TRADE_USDT = 10  # Amount of USDT to use per trade
-ATR_MULTIPLIER = 2
+ATR_MULTIPLIER = 1.5  # ATR multiplier for take profit calculation
 
 # Set leverage for the trading symbol
 def set_leverage(symbol, leverage):
